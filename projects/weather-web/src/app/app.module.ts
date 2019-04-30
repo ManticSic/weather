71be15c5fe56dc1df200dbfc94e423d67af5c73a
Darius Weber <darius.weber@posteo.de>
2019-04-30 15:32:01 +0200

    Import WeatherCoreModule

--- projects/weather-web/src/app/app.module.ts
+++ projects/weather-web/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WeatherCoreModule} from "weather-core";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeatherCoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
