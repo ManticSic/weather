84dbb550a0a96e20135ba6c40a09ee86660a3ab9
Darius Weber <darius.weber@posteo.de>
2019-04-30 17:30:08 +0200

    Add routing - landing view - forecast view

--- projects/weather-web/src/app/app.module.ts
+++ projects/weather-web/src/app/app.module.ts
@@ -3,12 +3,18 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {WeatherCoreModule} from "weather-core";
+import {WeatherWebRoutingModule} from "./routing.module";
+import {ForecastViewComponent} from './views/forecast-view/forecast-view.component';
+import { LandingViewComponent } from './views/landing-view/landing-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastViewComponent,
+    LandingViewComponent
   ],
   imports: [
+    WeatherWebRoutingModule,
     BrowserModule,
     WeatherCoreModule,
   ],
---
+++ projects/weather-web/src/app/views/forecast-view/forecast-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'weather-web-forecast-view',
+  templateUrl: './forecast-view.component.html',
+  styleUrls: ['./forecast-view.component.scss']
+})
+export class ForecastViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
