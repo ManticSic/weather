8c2c4a0eb124506898d910850345385bfe3c8e49
Darius Weber <darius.weber@posteo.de>
2019-05-01 14:01:54 +0200

    Get lat/long from url

--- projects/weather-web/src/app/views/forecast-view/forecast-view.component.ts
+++ projects/weather-web/src/app/views/forecast-view/forecast-view.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'weather-web-forecast-view',
   templateUrl: './forecast-view.component.html',
   styleUrls: ['./forecast-view.component.scss']
 })
-export class ForecastViewComponent implements OnInit {
+export class ForecastViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private destroy$: Subject<boolean> = new Subject();
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.route.paramMap
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(paramMap => {
+        const latParamValue: string = paramMap.get('lat');
+        const longParamValue: string = paramMap.get('long');
+
+        const letValue: number = +latParamValue;
+        const longValue: number = +longParamValue;
+
+        // todo nominatim reverse
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
