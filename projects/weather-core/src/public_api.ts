f0bcbee35829a4459835d0d03ff0b81b66024568
Darius Weber <darius.weber@posteo.de>
2019-05-01 14:11:18 +0200

    Add missing exports and rearrange exports (public api)

--- projects/weather-core/src/public_api.ts
+++ projects/weather-core/src/public_api.ts
@@ -7,6 +7,7 @@ export * from './lib/weather-core.module';
 export * from './lib/app-config.service'
 
 export * from './lib/models/app-config.interface';
+export * from './lib/models/cache-config.interface';
+export * from './lib/models/dark-sky-config.interface';
 export * from './lib/models/location.interface';
 export * from './lib/models/module-config.interface';
-export * from './lib/models/dark-sky-config.interface';
